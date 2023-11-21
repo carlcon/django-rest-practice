867cd86b040178bf1457fbac8f09d58eab6324f9
Julius Concepcion <julius@directworksmedia.com>
2023-11-22 02:57:08 +0800

    impelement token authentication and permissions

--- py_client/create.py
+++ py_client/create.py
@@ -1,6 +1,7 @@
 import requests
 
 
+headers = {'Authorization': 'Bearer 5266eccdba5e6b73c3f888be74c76ca2eec185df'}
 endpoint = "http://localhost:9000/api/products/"
 
 
@@ -9,7 +10,7 @@ data = {
     "price": 32.99
 }
 
-get_response = requests.post(endpoint, json=data)
+get_response = requests.post(endpoint, json=data, headers=headers)
 
 
 print(get_response.json())

e785ad6d847b2db48ce6ae360336fe228554bb87
Julius Concepcion <julius@directworksmedia.com>
2023-11-23 01:56:44 +0800

    custom validations implementation

--- backend/products/views.py
+++ backend/products/views.py
@@ -15,10 +15,8 @@ class ProductListCreateAPIView(
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
-        print(serializer.validated_data)
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content') or None
         if content is None:
